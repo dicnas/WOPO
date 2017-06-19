2aea50f3190316ebadbca72dc07368c6b60958d7
Bryan <bryanzt@live.com>
2017-06-19 08:54:31 -0300

    inicial

---
+++ src/app/home/home.js
@@ -0,0 +1,70 @@
+/**
+ * Each section of the site has its own module. It probably also has
+ * submodules, though this boilerplate is too simple to demonstrate it. Within
+ * `src/app/home`, however, could exist several additional folders representing
+ * additional modules that would then be listed as dependencies of this one.
+ * For example, a `note` section could have the submodules `note.create`,
+ * `note.delete`, `note.edit`, etc.
+ *
+ * Regardless, so long as dependencies are managed correctly, the build process
+ * will automatically take take of the rest.
+ *
+ * The dependencies block here is also where component dependencies should be
+ * specified, as shown below.
+ */
+angular.module('ngBoilerplate.home', [
+  'ui.router',
+  'plusOne'
+])
+
+  /**
+   * Each section or module of the site can also have its own routes. AngularJS
+   * will handle ensuring they are all available at run-time, but splitting it
+   * this way makes each module more "self-contained".
+   */
+  .config(function config($stateProvider) {
+    $stateProvider.state('home', {
+      url: '/home',
+      views: {
+        "main": {
+          controller: 'HomeCtrl',
+          templateUrl: 'home/home.tpl.html'
+        }
+      },
+      data: { pageTitle: 'Home' }
+    });
+  })
+.directive('customOnChange', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.bind('change', onChangeHandler);
+    }
+  };
+})
+  /**
+   * And of course we define a controller for our route.
+   */
+  .controller('HomeCtrl', function HomeController($scope) {
+
+    $scope.uploadFileListener = function (e) {
+      var files = e.target.files || e.dataTransfer.files;
+      console.log("ssff");
+      // process all File objects
+      for (var i = 0, f; f = files[i]; i++) {
+        console.log(
+          files[i]
+        );
+      }
+    };
+
+
+
+
+
+
+
+
+  });
+
